878d3ada1f9b88b38ad6c364247f05000669da32
Dima Pokusaev <36514128+DimaPokusaev@users.noreply.github.com>
2019-06-14 09:16:55 -0400

    Update HighScore.cs

--- GemCollector/HighScore.cs
+++ GemCollector/HighScore.cs
@@ -6,17 +6,16 @@ using System.Threading.Tasks;
 
 namespace GemCollector
 {
-    public class highScore
+    public class HighScore
     {
         public int time, clicks;
-        public string name, difficulty;
+        public string difficulty;
 
-        public highScore(string _difficulty, int _time, int _clicks, string _name)
+        public highScore(string _difficulty, int _time, int _clicks)
         {
             difficulty = _difficulty;
             time = _time;
             clicks = _clicks;
-            name = _name;
         }
     }
 }
